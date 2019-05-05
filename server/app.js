30c67d0eaac8561ef5efe69bee1f3a789b2e5970
GHDIShfdiod876dhs <30579663+GHDIShfdiod876dhs@users.noreply.github.com>
2019-05-04 23:08:36 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const cors = require('cors')
+const connectString = require('./connectString')
 
 const app = express()
 
@@ -15,7 +16,7 @@ mongoose.set('useNewUrlParser', true)
 mongoose.set('useFindAndModify', false)
 
 // connect to mongoDB Atlas database
-mongoose.connect('mongodb+srv://Chris:92DDTEtsqGjLiPc@traininglog0-k570t.mongodb.net/test?retryWrites=true')
+mongoose.connect(connectString)
 
 mongoose.connection.once('open', () => {
   console.log('You are connected to the database')
